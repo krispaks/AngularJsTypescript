ba89b479e59808ce0325bf5dea317ceb867a446a
krispaks <krispaks@yahoo.com>
2016-02-14 01:12:29 -0600

    some minor clean up

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,5 @@ import 'angular';
 
 import { TestingCtrl  } from './testing/controller/testingCtrl'; 
 
-export var app = angular.module('testing', []);
-
-app.controller('todoCtrl', TestingCtrl)
+export var app = angular.module('testing', [])
+        .controller('todoCtrl', TestingCtrl)
