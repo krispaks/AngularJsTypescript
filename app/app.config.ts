41b346b4aecbf0e876544f5752213a039bd4ecee
Khris Allan Paca <krispaks@yahoo.com>
2016-02-24 14:24:50 -0600

    some cleanup on route

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,25 +1,39 @@
+class HeroListRoute implements ng.route.IRoute {
+    template: string;
+    constructor(){
+        let route = this;
+        route.template = '<hero-list></hero-list>';
+    }
+}
+
+class HeroDetailRoute implements ng.route.IRoute {
+    template: string;
+    resolve: any;
+    
+    constructor(){
+        let route = this;
+        this.template = '<hero-detail hero="$resolve.hero"></hero-detail>';
+        this.resolve = {
+                        hero: function () {
+                            return {
+                                name: 'Captain Barbel',
+                                ability: 'Like Superman',
+                                strength: 1000    
+                            };
+                        }
+        }
+    }
+}
+
 export class AppConfig{
     'use strict';
     
     static $inject = ['$routeProvider', '$locationProvider'];
     
-    constructor($routeProvider: angular.route.IRouteProvider, $locationProvider: ng.ILocationProvider){
+    constructor($routeProvider: ng.route.IRouteProvider, $locationProvider: ng.ILocationProvider){
         $routeProvider
-        .when('/HeroList', {
-            template: '<hero-list></hero-list>'
-        })
-        .when('/HeroDetail', {
-            template: '<hero-detail hero="$resolve.hero"></hero-detail>',
-            resolve: {
-                hero: function () {
-                    return {
-                        name: 'Captain Barbel',
-                        ability: 'Like Superman',
-                        strength: 1000    
-                    };
-                }
-            }
-        });
+        .when('/HeroList', new HeroListRoute())
+        .when('/HeroDetail', new HeroDetailRoute());
         
         $locationProvider.html5Mode(true);
         $locationProvider.hashPrefix('!');
