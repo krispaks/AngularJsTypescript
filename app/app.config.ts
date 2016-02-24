e41db5a80342710d4e8924ab9ab7ecd31c5d0d15
krispaks <krispaks@yahoo.com>
2016-02-23 23:15:58 -0600

    attempt at component routing

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,9 +1,27 @@
 export class AppConfig{
     'use strict';
     
-    static $inject = ['$routeProvider'];
+    static $inject = ['$routeProvider', '$locationProvider'];
     
-    constructor($routeProvider: angular.route.IRouteProvider){
+    constructor($routeProvider: angular.route.IRouteProvider, $locationProvider: ng.ILocationProvider){
+        $routeProvider
+        .when('/HeroList', {
+            template: '<hero-list></hero-list>'
+        })
+        .when('/HeroDetail', {
+            template: '<hero-detail hero="$resolve.hero"></hero-detail>',
+            resolve: {
+                hero: function () {
+                    return {
+                        name: 'Captain Barbel',
+                        ability: 'Like Superman',
+                        strength: 1000    
+                    };
+                }
+            }
+        });
         
+        $locationProvider.html5Mode(true);
+        $locationProvider.hashPrefix('!');
     }
 }
