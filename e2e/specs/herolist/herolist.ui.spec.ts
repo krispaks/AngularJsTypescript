b87d6d268e0e99e603d3ecbdf0068a839acd5a41
Khris Allan Paca <krispaks@yahoo.com>
2016-03-01 16:54:57 -0600

    minor spec change

--- e2e/specs/herolist/herolist.ui.spec.ts
+++ e2e/specs/herolist/herolist.ui.spec.ts
@@ -4,11 +4,13 @@ describe('hero list test', () => {
    it('Hero List', function() {
     browser.get('http://127.0.0.1:8080');
     
-    browser.pause();
-    
-    var tabs = element.all(by.tagName('md-tabs'));
+    let tabs = element.all(by.tagName('md-tabs'));
     expect(tabs.count()).toEqual(1);
     
     tabs.get(0).element(by.tagName('a')).click(); 
+    
+    let items = element.all(by.className('md-fab'));
+    
+    browser.pause();
   });
 });
