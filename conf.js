67352e5dad435203e869d7724d587e8b16b60938
Khris Allan Paca <krispaks@yahoo.com>
2016-03-01 13:59:54 -0600

    ui spec stuff

--- conf.js
+++ conf.js
@@ -1,4 +1,9 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['./wwwroot/e2e/specs/herolist/herolist.ui.spec.js']
+    specs: ['./wwwroot/e2e/specs/herolist/herolist.ui.spec.js'],
+    getPageTimeout: 200000,
+    allScriptsTimeout: 200000,
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 200000
+    } 
 }
--- e2e/specs/herolist/herolist.ui.spec.ts
+++ e2e/specs/herolist/herolist.ui.spec.ts
@@ -4,15 +4,11 @@ describe('hero list test', () => {
    it('Hero List', function() {
     browser.get('http://127.0.0.1:8080');
     
-    var tabs = element.all(by.tagName('md-tabs'));
-    expect(tabs.count()).toEqual(3);
+    browser.pause();
     
-    tabs.get(1).element(by.tagName('a')).click();
-
-    var herolist = element.all(by.repeater('character in $ctrl.heroList'));
-    expect(herolist.count()).toEqual(3);
-
-    // You wrote your first test, cross it off the list
-    herolist.get(2).element(by.css('md-fab')).click();
+    var tabs = element.all(by.tagName('md-tabs'));
+    expect(tabs.count()).toEqual(1);
+    
+    tabs.get(0).element(by.tagName('a')).click(); 
   });
 });
