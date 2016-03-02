eb1c8c5a84886f964d3a8b79edc0025dfcb81af1
krispaks <krispaks@yahoo.com>
2016-03-02 07:10:18 -0600

    minor ui spec chanegs

--- conf.js
+++ conf.js
@@ -1,6 +1,7 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['./wwwroot/e2e/specs/herolist/herolist.ui.spec.js'],
+    specs: ['./wwwroot/e2e/specs/herolist/herolist.ui.spec.js'
+            , './wwwroot/e2e/specs/herodetail/herodetail.ui.spec.js'],
     getPageTimeout: 200000,
     allScriptsTimeout: 200000,
     jasmineNodeOpts: {
--- e2e/specs/herodetail/herodetail.ui.spec.ts
+++ e2e/specs/herodetail/herodetail.ui.spec.ts
@@ -1,19 +1,18 @@
 'use strict';
 
 describe('hero detail test', () => {
-   it('should add a todo', function() {
-    browser.get('https://angularjs.org');
-
-    element(by.model('todoList.todoText')).sendKeys('write first protractor test');
-    element(by.css('[value="add"]')).click();
-
-    var todoList = element.all(by.repeater('todo in todoList.todos'));
-    expect(todoList.count()).toEqual(3);
-    expect(todoList.get(2).getText()).toEqual('write first protractor test');
-
-    // You wrote your first test, cross it off the list
-    todoList.get(2).element(by.css('input')).click();
-    var completedAmount = element.all(by.css('.done-true'));
-    expect(completedAmount.count()).toEqual(2);
+   it('Hero Detail', function() {
+    browser.get('http://127.0.0.1:8080');
+    
+    let tabs = element.all(by.tagName('md-tab'));
+    expect(tabs.count()).toEqual(3);
+    browser.pause();
+    tabs.get(1).element(by.tagName('a')).click(); 
+    
+    let items = element.all(by.className('md-fab'));
+    
+    items.get(0).click();
+        
+    browser.sleep(20000);
   });
 });
